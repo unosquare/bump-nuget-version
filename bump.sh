f3e3003ad5b76154360fa0ce554e24efe4835a59
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2021-12-14 13:07:32 -0600

    Update bump.sh

--- bump.sh
+++ bump.sh
@@ -2,3 +2,4 @@ version=$(cat $csproj | sed -ne '/Version/{s/.*<Version>\(.*\)<\/Version>.*/\1/p
 oldVersion="<Version>$version</Version>"
 newVersion="<Version>$(semver -i $level $version)</Version>"
 sed -i "s|$oldVersion|$newVersion|" $csproj
+echo $newVersion
